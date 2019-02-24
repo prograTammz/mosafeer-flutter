f04945feb166eadc37aef972052a89060230f456
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2019-02-24 02:47:51 +0200

    added color platte

---
+++ mosafeer/lib/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kMosafeerPurple600 = const Color(0xFF720D5D);
+const kMosafeerPurple700 = const Color(0xFF720D5D);
+const kMosafeerPurple800 = const Color(0xFF5D1049);
+const kMosafeerPurple900 = const Color(0xFF4E0D3A);
+
+const kMosafeerRed700 = const Color(0xFFE30425);
+
+const kMosafeerPrimaryWhite = const Color(0xFFFFFFFF);
+const kMosafeerErrorOrange = const Color(0xFFFF9100);
+
+const kMosafeerAlpha = const Color(0x00FFFFFF);
+
+const kMosafeerGrey = Color(0xFF747474);
+
+const kMosafferBlac =Color(0xFF333333);
