94bfec526f0fba47302a93874d6c420936ac573c
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2019-02-27 00:54:55 +0200

    Added silver widgets

--- mosafeer/lib/home.dart
+++ mosafeer/lib/home.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 
+import 'colors.dart';
 import './components/TopAppBar.dart';
-
+import './model/CustomAppBar.dart';
 class HomePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        titleSpacing: 10.0,
-        backgroundColor: Theme.of(context).primaryColor,
-        flexibleSpace: TopAppBar(),
+      backgroundColor: kMosafeerPurple800,
+      body: CustomScrollView(
         
-      ),
-      body: Center(
-        child: Text("Shit"),
+        slivers: <Widget>[
+          CustomAppBar(),
+          SliverFillRemaining(
+            child: Container(
+              margin: EdgeInsets.only(top: 15.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0)
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 8.0,
+                    spreadRadius: 2.0,
+                    offset: Offset(0.0, -8.0)
+                  )
+                ]
+              ),
+            ),
+          )
+        ],
+
       ),
     );
   }
