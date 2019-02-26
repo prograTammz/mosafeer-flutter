405d0570ec3af70dd84b4ed2083330891ec059d2
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2019-02-27 00:55:12 +0200

    added debugging paint size

--- mosafeer/lib/main.dart
+++ mosafeer/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/rendering.dart';
 import 'app.dart';
 
 void main(){
+  debugPaintSizeEnabled=false;
   runApp(
     MosafeerApp()
   );
+  
 }
