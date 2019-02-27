46681b74c60a84a39884be55160047e8c663c88f
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2019-02-27 02:24:27 +0200

    Added scrollable menu tab views and refacotring

--- mosafeer/lib/model/CustomAppBar.dart
+++ mosafeer/lib/model/CustomAppBar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../components/TopAppBar.dart';
 
+import '../components/TopBarIndicator.dart';
+import '../colors.dart';
 class CustomAppBar extends StatefulWidget{
   
   @override 
@@ -23,25 +25,125 @@ class _CustomAppBarState extends State<CustomAppBar> with TickerProviderStateMix
   Widget build(BuildContext context){
     return SliverAppBar(
       floating: true,
-      expandedHeight: 104,
-      flexibleSpace: TopAppBar(tabController: _tabController),
-      bottom: PreferredSize(
-        preferredSize: Size(MediaQuery.of(context).size.width, 104.0),
-        child: TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            Container(
-              color: Colors.redAccent,
+      expandedHeight: 120,
+      leading: IconButton(
+                iconSize: 24.0,
+                padding: EdgeInsets.all(0.0),
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+                Align(
+                    alignment: Alignment.bottomRight,
+                    heightFactor: 1.4,
+                    child: Text(
+                      "مسافر",
+                      style: TextStyle(
+                          fontFamily: "Dima Kereshmeh",
+                          fontSize: 24,
+                          color: kMosafeerPrimaryWhite),
+                      textAlign: TextAlign.left,
+                    )),
+ 
+                Expanded(
+                  flex: 1,
+                  child: TabBar(
+                    indicator: CircularTabIndicator(),
+                    controller: _tabController,
+                    labelStyle:
+                        TextStyle(fontSize: 12, fontWeight: FontWeight.w700),
+                    tabs: <Widget>[
+                      Tab(
+                        text: "EXPLORE",
+                      ),
+                      Tab(
+                        text: "HANGOUT",
+                      ),
+                      Tab(
+                        text: "GUIDE",
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-            Container(
-              color: Colors.red[300],
+            bottom: PreferredSize(
+              preferredSize: Size(MediaQuery.of(context).size.width, 120),
+              child: Container(
+                height: 120,
+                padding: EdgeInsets.symmetric(horizontal: 4.0),
+                child: TabBarView(
+                  controller: _tabController,
+                  children: <Widget>[
+                    buildExploreMenu(context),
+                    buildHangoutMenu(context),
+                    buildGuideMenu(context)
+                  ],
+                ),
+              ),
             ),
-            Container(
-              color: Colors.blueAccent,
-            )
-          ],
-        ),
-      ),
-    );
+      );
+      
   }
+}
+Widget buildExploreMenu(context){
+
+  List<IconData> icons = [Icons.terrain,Icons.spa,Icons.place,Icons.local_library];
+  List<String> names = ["HISTORICAL","LEISURE","RELIGIOUS","CULTURE"];
+
+  return buildMenu(4,context, icons, names);
+}
+Widget buildHangoutMenu(context){
+
+  List<IconData> icons = [Icons.local_dining,Icons.pool,Icons.store_mall_directory,Icons.hotel];
+  List<String> names = ["RESTURANTS","SPORT","SHOPPING","HOTELS"];
+
+  return buildMenu(4,context, icons, names);
+
+}
+Widget buildGuideMenu(context){
+
+  List<IconData> icons = [Icons.attach_money,Icons.directions_bus,Icons.local_atm];
+  List<String> names = ["CURRENCY","TRANSPORT","PRICE GUIDE"];
+
+  return buildMenu(3,context, icons, names);
+}
+Widget buildMenu(count,context,icons,names){
+  return Center(
+    child: GridView.count(
+      crossAxisCount: 2,
+      physics: NeverScrollableScrollPhysics(),
+      childAspectRatio:((MediaQuery.of(context).size.width / 2) / 60),
+      shrinkWrap: true,
+      children: List.generate(count, (index){
+        return Container(
+          margin: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            color: Theme.of(context).accentColor,
+            boxShadow: [BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 2.0,
+                    spreadRadius: 1.0,
+                    offset: Offset(0.0, 3.0)
+                  )]
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Icon(icons[index]),
+              Text(names[index],style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.w700
+              ),)
+            ],
+          ),
+        );
+      }),
+    ),
+  );
 }
